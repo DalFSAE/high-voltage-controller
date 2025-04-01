73c5d8a840056fb6399573753140b5a70972845a
Ashton Dudley <adudley@dal.ca>
2025-03-31 22:25:17 -0300

    Started CAN driver

---
+++ Core/User/can_driver.c
@@ -0,0 +1,22 @@
+// can_driver.c
+
+
+#include "fdcan.h"
+
+#include "can_driver.h"
+
+
+
+extern FDCAN_HandleTypeDef hfdcan1;
+extern FDCAN_HandleTypeDef hfdcan2;
+
+
+void can_init(void) {
+    return;
+}
+
+void can_heatbeat(void) {
+    return;
+}
+
+
