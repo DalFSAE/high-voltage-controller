8bfb1c5e11bc3f717019137b559bc1b38fd4a59b
Ashton Dudley <adudley@dal.ca>
2026-01-22 23:43:01 -0400

    Add dummy failing test for GitHub Actions

--- Core/Test/test_hv_control_logic.c
+++ Core/Test/test_hv_control_logic.c
@@ -34,10 +34,16 @@ void test_init_state_outputs_are_safe_off(void)
     TEST_ASSERT_FALSE(out.air_p_on);
 }
 
+void test_dummy_failure(void)
+{
+    TEST_ASSERT_TRUE(false);  // always fails, just for testing gh action
+}
+
 int main(void)
 {
     UNITY_BEGIN();
     RUN_TEST(test_hvc_init_sets_defaults);
     RUN_TEST(test_init_state_outputs_are_safe_off);
+    RUN_TEST(test_dummy_failure);
     return UNITY_END();
 }
