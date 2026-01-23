24620fff2b137bbc187ee9735f1fd2f1aa4e54fc
Ashton Dudley <adudley@dal.ca>
2026-01-23 19:28:49 -0400

    Working RC circuit

--- Core/Test/mock_ts.h
+++ Core/Test/mock_ts.h
@@ -7,5 +7,5 @@ static inline float rc_step_vc(float v_prev, float v_in, float r_ohm, float c_f,
     float tau = r_ohm * c_f;
     float dt_s = delta_t_ms / 1000.0f;
     
-    return v_in + (dt_s / tau) * (v_prev - v_in);
+    return v_prev + (dt_s / tau) * (v_in - v_prev);
 }
--- Core/Test/test_hv_control_logic.c
+++ Core/Test/test_hv_control_logic.c
@@ -1,5 +1,8 @@
 #include "unity.h"
 #include "hv_control_logic.h"
+#include "mock_ts.h"
+#include "stdio.h"
+#include "unity_internals.h"
 
 void setUp(void) {}
 void tearDown(void) {}
@@ -34,6 +37,18 @@ void test_init_state_outputs_are_safe_off(void)
     TEST_ASSERT_FALSE(out.air_p_on);
 }
 
+void test_mock_ts(void) {
+    float v_in = 400.0f;
+    float t_delta_ms = 10;
+    float v_prev = 0.0f;
+
+    for (uint32_t t_ms = 0; t_ms <= 4000; t_ms += t_delta_ms) {
+        v_prev = rc_step_vc(v_prev, v_in, 2000.0f, 0.000440f, t_delta_ms);
+        printf("Time: %4d ms, Vc: %.2f V\n", t_ms, v_prev);
+    }
+    TEST_ASSERT_EQUAL(1, 1);
+}
+
 void test_mock_precharge_sequence(void) {
     HvcContext ctx;
     HvcOutputs out;
@@ -55,6 +70,7 @@ int main(void)
     UNITY_BEGIN();
     RUN_TEST(test_hvc_init_sets_defaults);
     RUN_TEST(test_init_state_outputs_are_safe_off);
+    RUN_TEST(test_mock_ts);
     RUN_TEST(test_mock_precharge_sequence);
     return UNITY_END();
 }
