01a2c39d09997910d889796155400fa711ae9449
Ashton Dudley <adudley@dal.ca>
2026-01-24 00:37:54 -0400

    Deleted old main

--- Core/Test/test_runner.c
+++ Core/Test/test_runner.c
@@ -10,14 +10,3 @@ int main(void)
     RUN_TEST(run_test_hv_control_logic);
     return UNITY_END();
 }
-
-
-// int main(void)
-// {
-//     UNITY_BEGIN();
-//     RUN_TEST(test_hvc_init_sets_defaults);
-//     RUN_TEST(test_init_state_outputs_are_safe_off);
-//     RUN_TEST(test_mock_ts);
-//     RUN_TEST(test_mock_precharge_sequence);
-//     return UNITY_END();
-// }
