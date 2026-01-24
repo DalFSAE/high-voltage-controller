8fde2132643fc6b4db9036111e1df0ed0231b2c6
Ashton Dudley <adudley@dal.ca>
2026-01-24 11:54:30 -0400

    Removed section and added comments

--- Core/User/integrator.h
+++ Core/User/integrator.h
@@ -3,17 +3,6 @@
 #include <stdbool.h>
 #include <stdint.h> 
 
-#ifndef MAX_ARRAY_LEN
-    #define MAX_ARRAY_LEN 128
-#endif
-
-// Soc Commands
-typedef enum {
-    ENABLE_TS = 0,
-    DISABLE_TS,
-    CLEAR_FAULT,
-} SocCmd_t;
-
 typedef struct {
     float x;
     uint32_t last_ms;
@@ -29,7 +18,7 @@ static inline float integrator_step_ms(Integrator *i, float rate, uint32_t now_m
     uint32_t dt_ms = now_ms - i->last_ms;  // compute dt first
     i->last_ms = now_ms;                   // then update timestamp
 
-    float dt_s = (float)dt_ms * 0.001f;
-    i->x += rate * dt_s;
+    float dt_s = (float)dt_ms * 0.001f;    // convert ms -> s 
+    i->x += rate * dt_s;                   // summation
     return i->x;
 }
