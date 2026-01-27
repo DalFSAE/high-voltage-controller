dad13710a17773dd8959a480c0507586301ddef8
ADUDLEY <ashtondudley03@gmail.com>
2026-01-26 23:45:28 -0400

    added crossover time

--- Core/User/hvc_config.h
+++ Core/User/hvc_config.h
@@ -21,8 +21,10 @@
 
 #define SDC_ADC_MIN_VOLTAGE 128
 #define SIMPLE_PC_LENGTH_MS 5000
-#define PC_TIMEOUT_MS 10000     // Throw a fault if PC is longer than value (ms)
-#define PC_MINTIME_MS 100       // Throw a fault if PC is shorter than value (ms)    
-#define PC_THRESH 0.90f         // Bring TS to 95% of the pack voltage
+#define PC_TIMEOUT_MS       10000       // Throw a fault if PC is longer than value (ms)
+#define PC_MINTIME_MS       100         // Throw a fault if PC is shorter than value (ms)    
+#define PC_OVERLAP_MS       10          // Allow AIR_N and the PC relay to both be on for (ms)
+#define PC_THRESH           0.90f       // Bring TS to % of the pack voltage
 
 #endif
+
